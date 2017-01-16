0f33aac0bef9874e490fd939605dd867ac8e2218
Thejass Krishnan <tkrishna@redhat.com>
2017-01-16 03:05:42 -0800

    Edited login.go - minor change

--- services/login/login.go
+++ services/login/login.go
@@ -1,7 +1,7 @@
 package services
 
  import (
-   "fmt"
+
    "golang.org/x/net/context"
    "encoding/base64"
    "errors"
@@ -11,7 +11,7 @@ package services
 type AuthServices struct{}
 
 func (m *AuthServices) Login(c context.Context, s *user.User) (*user.User, error) {
-	
+
   if(s.Username == "admin" && s.Password == "password"){
     tokStr := []byte(s.Username + ":" + s.Password)
     tokEnc := base64.StdEncoding.EncodeToString(tokStr)
