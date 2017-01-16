e8711b593d3358287bb38788104c68f9330fde8d
Thejass Krishnan <tkrishna@redhat.com>
2017-01-16 03:11:51 -0800

    typo

--- services/register.go
+++ services/register.go
@@ -41,7 +41,7 @@ func Init(){
   variables.Mux.Handle("/api/", gwmux)
   variables.Mux.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
 	variables.Mux.Handle("/views/", http.StripPrefix("/views/", http.FileServer(http.Dir("./public/views"))))
-	svariables.Mux.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./public/static/css"))))
+	variables.Mux.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./public/static/css"))))
 	variables.Mux.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./public/static/js"))))
 	variables.Mux.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./public/static/images"))))
   variables.Mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
